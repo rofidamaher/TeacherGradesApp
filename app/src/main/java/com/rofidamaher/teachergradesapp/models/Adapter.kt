b65b54c7ee9ee7d1297a29e8af7c479c191a54bd
Rofida Maher <rofidamaher97@gmail.com>
2020-03-29 21:06:58 +0200

    init

--- app/src/main/java/com/rofidamaher/teachergradesapp/models/Adapter.kt
+++ app/src/main/java/com/rofidamaher/teachergradesapp/models/Adapter.kt
@@ -7,7 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.rofidamaher.teachergradesapp.R
 import kotlinx.android.synthetic.main.row.view.*
 
-class Adapter(val mContext: ClassAFragment, val textNames:ArrayList<String>, val grades: ArrayList<String>) :
+class Adapter(val mContext: ClassAFragment, val studentList: Array<Student>) :
     RecyclerView.Adapter<Adapter.viewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -16,13 +16,13 @@ class Adapter(val mContext: ClassAFragment, val textNames:ArrayList<String>, val
     }
 
     override fun getItemCount(): Int {
-        return textNames.size
+        return studentList.size
         }
 
     override fun onBindViewHolder(holder: viewHolder, position: Int) {
 
-        holder.itemView.stu_name.text = textNames.get(position)
-        holder.itemView.stu_grade.text = grades.get(position)
+        holder.itemView.stu_name.text = studentList.get(position).Name
+        holder.itemView.stu_grade.text = studentList.get(position).Grade
        }
 
     public class viewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
--- app/src/main/java/com/rofidamaher/teachergradesapp/models/ClassAFragment.kt
+++ app/src/main/java/com/rofidamaher/teachergradesapp/models/ClassAFragment.kt
@@ -13,17 +13,13 @@ import kotlinx.android.synthetic.main.fragment_b.*
 
 class ClassAFragment : Fragment() {
 
-    var Names: ArrayList<String> = ArrayList()
-    var Grades: ArrayList<String> = ArrayList()
+    var students :Array<Student> = emptyArray()//List(25) {Student()}
+
 
     var newStu_name =""
     var newStu_grade =""
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_a, container, false)
 
         var name = arguments?.getString("Name")
@@ -38,13 +34,13 @@ class ClassAFragment : Fragment() {
             newStu_name = Enter_name.text.toString()
             newStu_grade = Enter_grade.text.toString()
 
-            Names.add(newStu_name)
-            Grades.add(newStu_grade)
-
-
+            students[0].Name=newStu_name
+            students[0].Grade=newStu_grade
         }
 
-        val adapter = Adapter(this, Names, Grades)
+
+        val adapter = Adapter(this, students)
+
         v.recycler.layoutManager = LinearLayoutManager(context)
         v.recycler.adapter = adapter
 
@@ -64,29 +60,21 @@ class ClassAFragment : Fragment() {
 
     private fun loadData() {
 
-        Names.add("Adel")
-        Grades.add("20")
-        Names.add("Ali")
-        Grades.add("50")
-        Names.add("Amir")
-        Grades.add("35")
-        Names.add("Adel")
-        Grades.add("20")
-        Names.add("Ali")
-        Grades.add("50")
-        Names.add("Amir")
-        Grades.add("35")
-        Names.add("Adel")
-        Grades.add("20")
-        Names.add("Ali")
-        Grades.add("50")
-        Names.add("Amir")
-        Grades.add("35")
-        Names.add("Adel")
-        Grades.add("20")
-        Names.add("Ali")
-        Grades.add("50")
-        Names.add("Amir")
-        Grades.add("35")
+        students[0].Name="ali"
+        students[0].Grade="20"
+        students[1].Name="Adel"
+        students[1].Grade="20"
+        students[2].Name="Ali"
+        students[2].Grade="50"
+        students[3].Name="Amir"
+        students[3].Grade="35"
+        students[4].Name="ali"
+        students[4].Grade="20"
+        students[5].Name="Adel"
+        students[5].Grade="20"
+        students[6].Name="Ali"
+        students[6].Grade="50"
+        students[7].Name="Amir"
+        students[7].Grade="35"
     }
 }
